ad213ee2f158e758ca641ad1dfcd8620dbe275d4
Mina S. Aziz <minaz922@gmail.com>
2020-12-21 04:54:35 +0200

    Add constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+// TODO: possibly get from env variables
+export const BASE_DOMAIN = 'http://short.est';
+export const POSSIBLE_CHARS =
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
